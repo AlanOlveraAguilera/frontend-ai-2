7a66297b9203799093553ec8bea1bbb703672bdf
AlanOlveraAguilera <alan.olbera.asd@gmail.com>
2024-12-12 03:26:51 -0600

    añado y enlazo el documento js

---
+++ Proyecto 2/app.js
@@ -0,0 +1,9 @@
+
+
+// const, var, let
+
+const bar = document.querySelector("#bar"); 
+const cross = document.querySelector("#cross");
+const nav = document.querySelector(".nav");
+
+bar.addEventListener
