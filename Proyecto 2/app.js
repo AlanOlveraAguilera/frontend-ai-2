4a66a381529191aa8fdc1f7c4a54394153b2f8d6
AlanOlveraAguilera <alan.olbera.asd@gmail.com>
2024-12-12 05:16:52 -0600

    cambio estructura css

--- Proyecto 2/app.js
+++ Proyecto 2/app.js
@@ -2,8 +2,14 @@
 
 // const, var, let
 
-const bar = document.querySelector("#bar"); 
-const cross = document.querySelector("#cross");
-const nav = document.querySelector(".nav");
+const bar = document.querySelector('#bar'); 
+const cross = document.querySelector('#cross');
+const nav = document.querySelector('.nav');
 
-bar.addEventListener
+bar.addEventListener('click', ()=>{
+  //  console.log('button has been clicked')
+
+  bar.style.display = 'none';
+  cross.style.display = 'block';
+
+} )
