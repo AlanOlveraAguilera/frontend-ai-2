f9231ece7a1ab2503455528b991007d438ffc1c4
AlanOlveraAguilera <alan.olbera.asd@gmail.com>
2024-12-16 00:39:23 -0600

    agrego una copia de mi trabajo final porque no se veia bien en github

---
+++ Proyecto 2.1/app.js
@@ -0,0 +1,19 @@
+
+
+// const, var, let
+
+const bar = document.querySelector('#bar'); 
+const cross = document.querySelector('#cross');
+const nav = document.querySelector('.nav');
+
+bar.addEventListener('click', () => {
+  bar.style.display = 'none';
+  cross.style.display = 'block';
+  nav.style.left = '20%' ;
+});
+
+cross.addEventListener('click', ()=>{
+    cross.style.display = 'none';
+    bar.style.display = 'block';
+    nav.style.left = '100%'; 
+})
